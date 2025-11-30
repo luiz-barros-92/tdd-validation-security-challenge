871110ec8c2140040708c5a770f8ce84154e0084
Luiz Barros <lck.barros@gmail.com>
2025-11-29 23:30:41 -0300

    environment variables, UserRepository, UserService, UserDetailsProjection and implement GrantedAuthority, UserDetails interfaces

--- src/main/java/com/devsuperior/bds04/entities/User.java
+++ src/main/java/com/devsuperior/bds04/entities/User.java
@@ -1,8 +1,12 @@
 package com.devsuperior.bds04.entities;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -14,9 +18,10 @@ import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
+@SuppressWarnings("serial")
 @Entity
 @Table(name = "tb_user")
-public class User {
+public class User implements UserDetails{
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -107,5 +112,15 @@ public class User {
 			return false;
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles;
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
 	}
 }
